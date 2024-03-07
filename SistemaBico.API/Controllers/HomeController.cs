8664fd908b55c495226a0776c023e17fce772a0f
gustavobr96 <58640061+gustavobr96@users.noreply.github.com>
2024-03-07 12:29:35 -0300

    add

--- SistemaBico.API/Controllers/HomeController.cs
+++ SistemaBico.API/Controllers/HomeController.cs
@@ -1,21 +1,48 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Sistema.Bico.Domain.Entities;
+using Sistema.Bico.Domain.Interface;
+using Sistema.Bico.Infra.Repository;
+using SistemaBico.API.Dtos;
+using SistemaBico.API.Dtos.ResponseRazor;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace SistemaBico.API.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IProdutoRepository _produtoRepository;
+        private readonly IMapper _mapper;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,
+            IProdutoRepository produtoRepository,
+            IMapper mapper)
         {
             _logger = logger;
+            _produtoRepository = produtoRepository;
+            _mapper = mapper;
         }
 
-        public IActionResult Index()
+
+        public IActionResult Login()
         {
             return RedirectToAction("index", "Login", new { area = "" });
         }
 
+        public async Task<IActionResult> Index()
+        {
+            var produtos = await ObterProdutos();
+            var produtosDto = _mapper.Map<List<ProdutoDto>>(produtos);
+
+            return View("index", new ListResponseRazor { Produtos = produtosDto });
+        }
+
+        private async Task<List<Produto>> ObterProdutos()
+        {
+            return await _produtoRepository.GetAll(x => x.Ativo);
+        }
     }
 }
