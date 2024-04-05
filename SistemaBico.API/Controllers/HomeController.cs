66c2f7ee00751d67c159a7fc4cd9a3567ba9f297
gustavobr96 <58640061+gustavobr96@users.noreply.github.com>
2024-04-05 11:44:19 -0300

    aa

--- SistemaBico.API/Controllers/HomeController.cs
+++ SistemaBico.API/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Ardalis.Result;
 using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
--- SistemaBico.API/Controllers/MenusController.cs
+++ SistemaBico.API/Controllers/MenusController.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Sistema.Bico.Domain.Command;
 using Sistema.Bico.Domain.Command.Menu;
@@ -15,6 +16,7 @@ using System.Threading.Tasks;
 
 namespace SistemaBico.API.Controllers
 {
+    [Authorize]
     [Route("[controller]")]
     public class MenusController : Controller
     {
